9649e0d23ac26e2a6afc21b75d04f2d165fb0783
Nick Edwards <nedwards@cern.ch>
2015-07-14 17:25:50 +0100

    Adding script to get inputs

---
+++ get_inputs.sh
@@ -0,0 +1,6 @@
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/tauphobic_8TeV_ataueq0_mu2000.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/tauphobic_8TeV_tanbHigh_ataueqat_mu2000.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/out.lightstau1-8TeV-tanbHigh-nnlo.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/out.mhmodp-8TeV-tanbHigh-nnlo.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/out.mhmodm-8TeV-tanbHigh-nnlo.root
+wget https://twiki.cern.ch/twiki/pub/LHCPhysics/LHCHXSWGMSSMNeutral/out.lightstopmod-8TeV-tanbHigh-nnlo.root
